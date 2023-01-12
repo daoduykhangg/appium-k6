3221833057f916fd42b5394c0b0a84b59137365b
KhangD <Khang.DaoD@homecredit.vn>
2023-01-12 16:41:23 +0700

    Lesson15

---
+++ src/test/java/driver/DriverFactory.java
@@ -0,0 +1,48 @@
+package driver;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory implements MobileCapabilityTypeEx {
+    public static AppiumDriver<MobileElement> getDriver(Platforms platform) {
+        if (platform == null) {
+            throw new IllegalArgumentException("Platform can't be null, you can provide one of these:" + Arrays.toString(Platforms.values()));
+        }
+
+        AppiumDriver<MobileElement> appiumDriver = null;
+        Exception exception = null;
+        try {
+            DesiredCapabilities desiredCaps = new DesiredCapabilities();
+            desiredCaps.setCapability(PLATFORM_NAME, "Android");
+            desiredCaps.setCapability(AUTOMATION_NAME, "uiautomator2");
+            desiredCaps.setCapability(UDID, "emulator-5554");
+            desiredCaps.setCapability(APP_PACKAGE, "com.wdiodemoapp");
+            desiredCaps.setCapability(APP_ACTIVITY, ".MainActivity t13");
+
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            switch (platform) {
+                case android:
+                    appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCaps);
+                    break;
+                case ios:
+                    appiumDriver = new IOSDriver<MobileElement>(appiumServer, desiredCaps);
+                    break;
+            }
+
+            appiumDriver.manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            exception = e;
+        }
+        if (appiumDriver == null) {
+            throw new RuntimeException(exception.getMessage());
+        }
+        return appiumDriver;
+    }
+}
---
+++ src/test/java/test/FormInteraction.java
@@ -0,0 +1,37 @@
+package test;
+
+import driver.DriverFactory;
+import driver.Platforms;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FormInteraction {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> driver;
+        driver = DriverFactory.getDriver(Platforms.android);
+        try {
+            MobileElement navLoginBtnElem = driver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginBtnElem.click();
+
+            MobileElement usernameElem = driver.findElement(MobileBy.AccessibilityId("input-email"));
+            usernameElem.sendKeys("abc@gmail.com");
+
+            MobileElement passwordElem = driver.findElement(MobileBy.AccessibilityId("input-password"));
+            passwordElem.sendKeys("12345678");
+
+            MobileElement loginBtnElem = driver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
+            loginBtnElem.click();
+            WebDriverWait explicitWait = new WebDriverWait(driver, 10);
+            explicitWait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.id("android:id/alertTitle")));
+            MobileElement loginDialogTitleElem = driver.findElement(MobileBy.id("android:id/alertTitle"));
+            System.out.println(loginDialogTitleElem.getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+}
